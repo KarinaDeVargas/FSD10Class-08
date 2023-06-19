f4a2fca6bfcf4a36ab5e4260deec88053238c86d
KarinaDeVargas <kavp86@gmail.com>
2023-06-19 14:33:30 -0400

    this is my message. Adding files for Class 08

---
+++ MathClass.java
@@ -0,0 +1,16 @@
+package June08th;
+
+import java.util.Scanner;
+
+public class MathClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		
+		
+		
+	}
+
+}
